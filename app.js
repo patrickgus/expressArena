3d65fd7ec49d6f0ee50817fd5c47199a5446a7f5
Patrick Gustafson <patrickgus@charter.net>
2019-12-23 11:12:28 -0800

    add more examples

--- app.js
+++ app.js
@@ -30,32 +30,99 @@ app.get("/echo", (req, res) => {
   res.send(responseText);
 });
 
-app.get('/queryViewer', (req, res) => {
+app.get("/queryViewer", (req, res) => {
   console.log(req.query);
   res.end(); //do not send any data back to the client
 });
 
-app.get('/greetings', (req, res) => {
+app.get("/greetings", (req, res) => {
   //1. get values from the request
   const name = req.query.name;
   const race = req.query.race;
 
   //2. validate the values
-  if(!name) {
+  if (!name) {
     //3. name was not provided
-    return res.status(400).send('Please provide a name');
+    return res.status(400).send("Please provide a name");
   }
 
-  if(!race) {
+  if (!race) {
     //3. race was not provided
-    return res.status(400).send('Please provide a race');
+    return res.status(400).send("Please provide a race");
   }
 
   //4. and 5. both name and race are valid so do the processing.
   const greeting = `Greetings ${name} the ${race}, welcome to our kingdom.`;
 
-  //6. send the response 
+  //6. send the response
   res.send(greeting);
+});
+
+app.get("/hello", (req, res) => {
+  res.status(204).end();
+});
+
+app.get("/video", (req, res) => {
+  const video = {
+    title: "Cats falling over",
+    description: "15 minutes of hilarious fun as cats fall over",
+    length: "15.40"
+  };
+  res.json(video);
+});
+
+app.get("/colors", (req, res) => {
+  const colors = [
+    {
+      name: "red",
+      rgb: "FF0000"
+    },
+    {
+      name: "green",
+      rgb: "00FF00"
+    },
+    {
+      name: "blue",
+      rgb: "0000FF"
+    }
+  ];
+  res.json(colors);
+});
+
+app.get("/grade", (req, res) => {
+  // get the mark from the query
+  const { mark } = req.query;
+
+  // do some validation
+  if (!mark) {
+    // mark is required
+    return res.status(400).send("Please provide a mark");
+  }
+
+  const numericMark = parseFloat(mark);
+  if (Number.isNaN(numericMark)) {
+    // mark must be a number
+    return res.status(400).send("Mark must be a numeric value");
+  }
+
+  if (numericMark < 0 || numericMark > 100) {
+    // mark must be in range 0 to 100
+    return res.status(400).send("Mark must be in range 0 to 100");
+  }
+
+  if (numericMark >= 90) {
+    return res.send("A");
+  }
+
+  if (numericMark >= 80) {
+    return res.send("B");
+  }
+
+  if (numericMark >= 70) {
+    return res.send("C");
+  }
+
+  res.send("F");
 });
 
 app.listen(8000, () => {
